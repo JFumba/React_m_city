b551dbc94c00cb2c6d36cd96b3853d1e4c50615a
Johnson <jessefumba@gmail.com>
2018-11-22 16:53:01 +0100

    dbt firebase

---
+++ m-city/src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/database';
+
+const config = {
+    apiKey: "AIzaSyC9C2Lifhg9BMTaH95nUfh3I_3d7lPrs_o",
+    authDomain: "m-city-c0dac.firebaseapp.com",
+    databaseURL: "https://m-city-c0dac.firebaseio.com",
+    projectId: "m-city-c0dac",
+    storageBucket: "m-city-c0dac.appspot.com",
+    messagingSenderId: "482103258512"
+  };
+
+
+  firebase.initializeApp(config);
+
+  const firebaseDB = firebase.database();
+  const firebaseMatches = firebaseDB.ref('matches');
+
+  export {
+      firebase,
+      firebaseMatches
+  }
