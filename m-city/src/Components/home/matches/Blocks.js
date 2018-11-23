e0d314af5debb89a069db000a658e4770e2f3121
Johnson <jessefumba@gmail.com>
2018-11-23 12:18:44 +0100

    matches done meet players in progress

--- m-city/src/Components/home/matches/Blocks.js
+++ m-city/src/Components/home/matches/Blocks.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { firebaseMatches} from '../../../firebase';
+import { firebaseLooper, reverseArray } from '../../ui/misc';
+
+import MatchesBlock from '../../ui/matches_block';
+import Slide from 'react-reveal/Slide';
 
 class Blocks extends Component {
 
@@ -8,18 +12,31 @@ class Blocks extends Component {
     }
 
     componentDidMount(){
-        firebaseMatches.limitToLast(6).once('value').then((snapchot)=>{
-            
+        firebaseMatches.limitToLast(6).once('value').then((snapshot)=>{
+            const matches = firebaseLooper(snapshot);
+
+            this.setState({
+                matches: reverseArray(matches)
+            });
         })
     }
 
-    showMatches = () => (
-        <div>
-            match
-        </div>
+    showMatches = (matches) => (
+        matches ?
+            matches.map((match) => (
+                <Slide bottom key={match.id}>
+                    <div className="item">
+                        <div className="wrapper">
+                            <MatchesBlock match={match}/>
+                        </div>
+                    </div>
+                </Slide>
+            ))
+        :null
     )
 
     render() {
+        console.log(this.state)
         return (
             <div className="home_matches">
             {this.showMatches(this.state.matches)}
